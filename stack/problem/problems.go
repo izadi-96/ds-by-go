d3596854ff0f1c6b673718679f69b04abb2fee9c
Ali Izadi <al.izadi@mydigipay.com>
2024-07-05 10:07:50 +0330

    implement CheckStringLiteral with stack

--- stack/problem/problems.go
+++ stack/problem/problems.go
@@ -1,5 +1,9 @@
 package problem
 
+import (
+	"ds-by-go/stack"
+)
+
 type pair struct {
 	open  rune
 	close rune
@@ -11,6 +15,24 @@ var pairs = []pair{
 	{'{', '}'},
 }
 
-func CheckStringLiteral(s string) bool {
-	return false
+// CheckStringLiteral PROBLEM_01 checking balancing of symbols/parentheses
+func CheckStringLiteral(str string) bool {
+	stack := stack.NewDynamicStack[string](1)
+	for _, r := range str {
+		for _, p := range pairs {
+			temp, _ := stack.Peek()
+			if r == p.open {
+				stack.Push(r)
+				break
+			} else if r == p.close && stack.IsEmpty() {
+				return false
+			} else if r == p.close && temp == p.open {
+				stack.Pop()
+				break
+			} else if r == p.close && temp != p.open {
+				return false
+			}
+		}
+	}
+	return stack.IsEmpty()
 }
--- stack/problem/problems_test.go
+++ stack/problem/problems_test.go
@@ -1,1 +1,25 @@
 package problem
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCheckStringLiterals(t *testing.T) {
+	testCases := []struct {
+		input  string
+		result bool
+	}{
+		{input: "()", result: true},
+		{input: "(]", result: false},
+		{input: "", result: true},
+		{input: "(())()[]", result: true},
+		{input: ")", result: false},
+	}
+
+	for _, tc := range testCases {
+		result := CheckStringLiteral(tc.input)
+		assert.Equal(t, tc.result, result)
+	}
+
+}
